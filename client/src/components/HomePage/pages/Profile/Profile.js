9131d63e48fb7e007346b810fe07089577ba5db4
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-06-16 18:19:46 -0500

    restyled profile page

--- client/src/components/HomePage/pages/Profile/Profile.js
+++ client/src/components/HomePage/pages/Profile/Profile.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import jwtDecode from 'jwt-decode';
 import './profile.css';
+import pfp from '../../../photos/pfp placeholder.png';
+import Posts from './Posts/Posts';
+import MyRecordings from './myRecordings/myRecordings';
 
 const GET_USER = gql`
   query GetUser($userId: ID!) {
@@ -17,6 +20,22 @@ const Profile = () => {
   const decodedToken = jwtDecode(token);
   const userId = decodedToken.data._id;
   console.log("userId:", userId);
+
+  const [botHalf, setBotHalf] = useState("Posts");
+
+  const botHalfRender = () => {
+    if (botHalf === "Posts") {
+      return <Posts />;
+    }
+    if (botHalf === "myRecordings") {
+      return <MyRecordings />;
+    }
+
+  };
+
+  const handleBotHalfChange = (prop) => {
+    setBotHalf(prop);
+  };
 
   const { data, loading, error } = useQuery(GET_USER, {
     variables: {
@@ -41,20 +60,30 @@ const Profile = () => {
     return <div>Error: {error.message}</div>;
   }
 
+
+  // <div className="userProfileInfo">
+  //           <img className="profilePfp" src={data.getUser.profilePicture} alt="Profile Picture" />
+  //           <div className="profileUsername">{data.getUser.username}</div>
+  //           <p className="profileBio">{data.getUser.bio}</p>
+  //         </div>
   return (
     <>
       <div className="profileHeader">Your Profile</div>
       <div className="mainProfileContainer">
         {/* Render the user data */}
         {data && data.getUser && (
-          <div className="userProfileInfo">
-            {/* <img className="profilePfp" src={data.getUser.profilePicture} alt="Profile Picture" /> */}
-            <div className="profileUsername">{data.getUser.username}</div>
-            <p className="profileBio">{data.getUser.bio}</p>
-          </div>
+            <div className="userProfileInfo">
+              <img className="profilePfp" src={pfp}></img>
+              {/* <div className="profileName">FName LName</div> */}
+              <div className="profileUsername">{`@${data.getUser.username}`}</div>
+              <p className="profileBio">
+                {data.getUser.bio}
+              </p>
+            </div>
         )}
       </div>
     </>
+    
   );
 };
 
